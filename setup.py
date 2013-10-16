e17b917d219de4e7cb1e1b2940276f97acd21ccb
hvelarde <hector.velarde@gmail.com>
2013-10-16 11:22:06 -0300

    Fix package distribution

--- setup.py
+++ setup.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
-# $Id$
+
+from setuptools import find_packages
+from setuptools import setup
+
 import os
-from setuptools import setup, find_packages
 
 
 def _textOfModuleFile(filename):
@@ -13,36 +15,54 @@ long_description = _textOfModuleFile(os.path.join('Products', 'windowZ',
 long_description += '\n\n'
 long_description += _textOfModuleFile(os.path.join('docs', 'INSTALL.txt'))
 long_description += '\n\n'
-long_description += _textOfModuleFile(os.path.join('docs', 'HISTORY.txt'))
+long_description += _textOfModuleFile('CHANGES.rst')
 long_description += '\n\n'
-long_description += _textOfModuleFile(os.path.join('docs', 'AUTHORS.txt'))
+long_description += _textOfModuleFile('CONTRIBUTORS.rst')
 long_description += '\n\n'
 
 setup(name='Products.windowZ',
       version=version,
-      description="Show web-pages inside an iframe. Plone content-type.",
+      description='Show web-pages inside an iframe. Plone content-type.',
       long_description=long_description,
-      # Get more strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
-          "Programming Language :: Python",
-          "Framework :: Plone",
-          "License :: OSI Approved :: GNU General Public License (GPL)",
-          "Development Status :: 5 - Production/Stable",
-          ],
+          'Development Status :: 6 - Mature',
+          'Environment :: Web Environment',
+          'Framework :: Plone',
+          'Framework :: Plone :: 4.3',
+          'License :: OSI Approved :: GNU General Public License (GPL)',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 2.7',
+          'Topic :: Software Development :: Libraries :: Python Modules',
+      ],
       keywords='plone layout composition themeing',
       author='Jean Rodrigo Ferri',
       author_email='jeanrodrigoferri@yahoo.com.br',
-      url='http://www.plone.org/products/windowz',
+      url='http://plone.org/products/windowz',
       license='GPL',
       packages=find_packages(exclude=['ez_setup']),
       namespace_packages=['Products'],
       include_package_data=True,
       zip_safe=False,
       install_requires=[
+          'plone.app.controlpanel',
+          'plone.fieldsets',
+          'Products.Archetypes',
+          'Products.ATContentTypes',
+          'Products.CMFCore',
+          'Products.GenericSetup',
           'setuptools',
-          # -*- Extra requirements: -*-
           'stripogram',
+          'zope.i18nmessageid',
+          'zope.interface',
+          'zope.schema',
       ],
+      extras_require={
+          'test': [
+              'Products.PloneTestCase',
+              'Testing.ZopeTestCase',
+          ],
+      },
       entry_points="""
       # -*- Entry points: -*-
       """,
