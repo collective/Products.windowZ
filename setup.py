2cb13f44fb422a4d6220520289483678df4c27ac
MrTango <md@derico.de>
2019-10-30 12:10:23 +0200

    Back to development: 2.0.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 import os
 
-version = '2.0.1'
+version = '2.0.2.dev0'
 long_description = (
     open('README.rst').read() + '\n' +
     open(os.path.join('docs', 'INSTALL.txt')).read() + '\n' +
